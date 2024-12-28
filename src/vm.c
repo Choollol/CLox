5a430cfaba20092bf79cea85301ab56fc8d5f0fe
Choollol <67297627+Choollol@users.noreply.github.com>
2024-12-28 10:32:46 -0800

    Arithmetic operators

--- src/vm.c
+++ src/vm.c
@@ -20,6 +20,12 @@ void freeVM() {
 
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while(false)
 
 void printStack() {
     printf("          ");
@@ -44,6 +50,21 @@ static InterpretResult run() {
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
+            case OP_ADD:
+                BINARY_OP(+);
+                break;
+            case OP_SUBTRACT:
+                BINARY_OP(-);
+                break;
+            case OP_MULTIPLY:
+                BINARY_OP(*);
+                break;
+            case OP_DIVIDE:
+                BINARY_OP(/);
+                break;
+            case OP_NEGATE:
+                *(vm.stackTop - 1) = -*(vm.stackTop - 1);
+                break;
             case OP_RETURN:
                 printValue(pop());
                 printf("\n");
@@ -54,6 +75,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 
 InterpretResult interpret(Chunk* chunk) {
     vm.chunk = chunk;

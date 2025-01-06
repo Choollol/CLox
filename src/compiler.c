61ecdc72e08c1dfd01aff4e178a7a54629aff424
Choollol <67297627+Choollol@users.noreply.github.com>
2025-01-05 18:17:59 -0800

    Get and set expressions

--- src/compiler.c
+++ src/compiler.c
@@ -497,6 +497,20 @@ static void call(bool canAssign) {
     emitBytes(OP_CALL, argCount);
 }
 
+/// @brief Parses a dot expression.
+static void dot(bool canAssign) {
+    consume(TOKEN_IDENTIFIER, "Expect property name after '.'.");
+    uint8_t name = identifierConstant(&parser.previous);
+
+    if (canAssign && match(TOKEN_EQUAL)) {
+        expression();
+        emitBytes(OP_SET_PROPERTY, name);
+    }
+    else {
+        emitBytes(OP_GET_PROPERTY, name);
+    }
+}
+
 /// @brief Parses a non-number literal expression.
 static void literal(bool canAssign) {
     switch (parser.previous.type) {
@@ -588,7 +602,7 @@ ParseRule rules[] = {
     [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
-    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, dot, PREC_CALL},
     [TOKEN_MINUS] = {unary, binary, PREC_TERM},
     [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
     [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
